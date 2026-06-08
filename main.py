bb71b2a046333112e83d0f52aa2320239e997d6c
nandanaabhay <nandanaabhay@gmail.com>
2026-06-08 20:01:12 +0530

    refactor: organize input paths and clean up inference calls

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from inference import run_inference, run_live_detection
 
-# input_url = "input/image/traffic.jpg"
-# result_dir = run_inference(input_url)
+# Input Video/Image Path
+input_url = "input/video/beach.mp4"
+input_url2 = "input/image/traffic.jpg"
+input_url3 = "input/image/birthday.webp"
 
-run_live_detection()
+# Run Inference for image/video output
+run_inference("input/video/beach.mp4")
+# run_inference(input_url3)
+
+# Live Detection
+# run_live_detection()
 
 
