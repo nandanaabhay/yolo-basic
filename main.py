1b7890c8c90935a320451954ace4787af2a49260
nandanaabhay <nandanaabhay@gmail.com>
2026-06-08 20:02:32 +0530

    live detection

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ input_url2 = "input/image/traffic.jpg"
 input_url3 = "input/image/birthday.webp"
 
 # Run Inference for image/video output
-run_inference("input/video/beach.mp4")
+# run_inference("input/video/beach.mp4")
 # run_inference(input_url3)
 
 # Live Detection
-# run_live_detection()
+run_live_detection()
 
 
